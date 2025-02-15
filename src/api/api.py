b49b08c4794ab1f892a4dbbd3d8e3246e32a1f68
TSKM5 <47384417+TSKM5@users.noreply.github.com>
2025-02-15 16:33:00 +1100

    initing api

--- src/api/api.py
+++ src/api/api.py
@@ -1,8 +1,17 @@
-from src import app
+from src import app, socketio
 from src.api.rest.score_api import score_blueprint
+from src.api.ws.score_ws import init_score_ws
 
 
 def init_api():
+    #all blueprints go here 
     app.register_blueprint(score_blueprint)
+
+    
+    
+    #init the ws
+    init_score_ws(socketio)
+
+    
     
     
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from . import app, socketio
 import logging
+from .api.api import init_api
 
 if __name__ == '__main__':
     if __debug__: 
@@ -7,3 +8,4 @@ if __name__ == '__main__':
     else:
         logging.basicConfig(level=logging.INFO)
     socketio.run(app, host='0.0.0.0', port=7373, debug=True)
+    init_api()
